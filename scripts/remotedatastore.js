cb7923b6ef71ba6d160706a64476a621444ec663
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-10-09 16:36:53 -0700

    completed hw4

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -10,93 +10,109 @@
     this.serverUrl = url;
   }
 
-
-  // RemoteDataStore.prototype.add = function(key, val) {
-  //   $.post(this.serverUrl, val, function (serverResponse){
-  //     console.log(serverResponse);
-  //   });
-  // };
-
-  //Retrieve data from serverUrl
-  // RemoteDataStore.prototype.getAll = function(cb){
-  //   $.get(this.serverUrl, function (serverResponse){
-  //     console.log(serverResponse);
-  //     cb(serverResponse);
-  //   });
-  // };
-
-  //Call Back
-  // RemoteDataStore.prototype.get = function(key, cb){
-  //   $.get(this.serverUrl + '/' + key, function (serverResponse){
-  //     console.log(serverResponse);
-  //     cb(serverResponse);
-  //   });
-  // };
-
-  //Remove Data
-  // RemoteDataStore.prototype.remove = function(key){
-  //   $.ajax(this.serverUrl + '/' + key,{
-  //     type: 'DELETE'
-  //   });
-  // };
-  // RemoteDataStore.prototype.get = function(key){
-  //
-  // };
-
-  RemoteDataStore.prototype.getAll = function(key){
-   $.ajax(this.serverUrl,{
-     type:'GET',
-     success: function(serverResponse){
-       console.log(serverResponse);
-     },
-     error: function(xhr){
-       console.log(xhr.responseText);
-     }
-   });
-  };
-
-  RemoteDataStore.prototype.getId = function(key){
-    $.ajax(this.serverUrl,{
-      type:'GET',
-      contentType: 'application/json';
-      data: JSON.stringify({id : key}),
-      success: function(){
-
+  //get all information in server
+  RemoteDataStore.prototype.getAll = function(key) {
+    $.ajax(this.serverUrl, {
+      type: 'GET',
+      success: function(serverResponse) {
+        console.log(serverResponse);
       },
-      error: function(xhr){
+      error: function(xhr) {
         console.log(xhr.responseText);
       }
     });
   };
 
-  RemoteDataStore.prototype.add = function(key,val){
-   $.ajax(this.serverUrl,{
-     type:'POST',
-     contentType: 'application/json',
-     data: JSON.stringify({emailCustomer : key, coffeeOrder : val.coffee}),
-     success: function(){
+  //Get coffee Order by email
+  RemoteDataStore.prototype.get = function(key) {
+    $.ajax(this.serverUrl, {
+      type: 'GET',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        emailCustomer: key
+      }),
+      success: function(serverResponse) {
+        console.log(serverResponse);
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
+    });
+  };
 
-     },
-     error: function(xhr){
-       console.log(xhr.responseText);
-     }
-   });
- };
+  //Add coffee order to server
+  RemoteDataStore.prototype.add = function(key, val) {
+    $.ajax(this.serverUrl, {
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        emailCustomer: key,
+        coffeeOrder: val.coffee
+      }),
+      success: function() {},
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
+    });
+  };
 
- RemoteDataStore.prototype.remove = function(key){
-   var homeurl = this.serverUrl;
-   $.ajax({
-     type:'POST',
-     url: homeurl,
-     data: {_method:"DELETE"},
-     success: function(serverResponse){
-       console.log(serverResponse);
-     },
-     error: function(xhr){}
-   });
- };
+  //Remove coffeeOrder from server
+  RemoteDataStore.prototype.remove = function(key) {
+    var collectionUrl = this.serverUrl;
+    var response = $.ajax(this.serverUrl, {
+      type: 'GET',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        emailCustomer: key
+      }),
+      success: function() {
+        var getid = response.responseJSON[0].id;
+        $.ajax({
+          type: 'POST',
+          url: collectionUrl + '/' + getid,
+          data: {
+            _method: "DELETE"
+          }
+        });
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText);
+      }
+    });
 
+  };
 
+    // RemoteDataStore.prototype.add = function(key, val) {
+    //   $.post(this.serverUrl, val, function (serverResponse){
+    //     console.log(serverResponse);
+    //   });
+    // };
+
+    //Retrieve data from serverUrl
+    // RemoteDataStore.prototype.getAll = function(cb){
+    //   $.get(this.serverUrl, function (serverResponse){
+    //     console.log(serverResponse);
+    //     cb(serverResponse);
+    //   });
+    // };
+
+    //Call Back
+    // RemoteDataStore.prototype.get = function(key, cb){
+    //   $.get(this.serverUrl + '/' + key, function (serverResponse){
+    //     console.log(serverResponse);
+    //     cb(serverResponse);
+    //   });
+    // };
+
+    //Remove Data
+    // RemoteDataStore.prototype.remove = function(key){
+    //   $.ajax(this.serverUrl + '/' + key,{
+    //     type: 'DELETE'
+    //   });
+    // };
+    // RemoteDataStore.prototype.get = function(key){
+    //
+    // };
 
   App.RemoteDataStore = RemoteDataStore;
   window.App = App;
